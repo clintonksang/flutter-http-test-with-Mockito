150342a91047ec857a8daf26a03532186dbf6ef7
clintonksang <sangclint0@gmail.com>
2022-02-01 06:06:17 +0300

    simple setup

---
+++ test/new_widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutterdevtest/new_widget.dart';
+
+void main() {
+  testWidgets('Finds title, message', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyWidget(title: 'T', message: 'M'));
+
+    final titleFinder = find.text('T');
+    final messageFinder = find.text('M');
+
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
+  });
+}
